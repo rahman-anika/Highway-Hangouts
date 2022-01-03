db2e056dd8a01f456680fa937e5a5551928050b8
JoynoolAbedinMithoon <mithun55@gmail.com>
2022-01-03 22:56:55 +0600

    booking status added

--- src/components/Reservation/Confirm.js
+++ src/components/Reservation/Confirm.js
@@ -30,10 +30,12 @@ function Confirm ()
             book['booking_date'] = reservation.date;
             book['booking_time'] = reservation.time;
             book['guest_number'] = reservation.guest;
+            book['status'] = 'pending';
         }
         else {
             book['booking_date'] = reservation.date;
             book['booking_time'] = reservation.time;
+            book['status'] = 'pending';
         }
 
         fetch('http://localhost:5000/booking', {
@@ -48,7 +50,7 @@ function Confirm ()
             {
                 if (data.insertedId) {
                     Swal.fire(
-                        'Thank You!',
+                        `Thank You, ${user.displayName}`,
                         `Your table booking has been confirmed at ${reservation.time} on ${reservation.date}.`,
                         'success'
                     );
