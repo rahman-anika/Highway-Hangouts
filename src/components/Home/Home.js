f6cbefd07dc844612951e2b364111bb6c2981183
rahman-anika <anikarahman37@gmail.com>
2022-01-03 13:12:29 +0600

    home modified

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,44 @@
 import React from 'react';
+import Footer from '../../Shared/Footer/Footer';
+import Header from '../../Shared/Header/Header';
+import About from '../About/About';
+import Banner from '../Banner/Banner';
+import Chef from '../Chef/Chef';
+import Menu from '../Menu/menu';
+
+
+
 
 const Home = () => {
     return (
-        <div>
+        <div id="home">
+
+            {/* header section starts */}
+            <Header></Header>
+            {/* header section ends */}
+
+            {/* banner section starts */}
+            <Banner></Banner>
+            {/* banner section ends */}
+
+            {/* menu section starts */}
+            <Menu></Menu>
+            {/* menu section ends */}
+
+            {/* chef section starts */}
+            <Chef></Chef>
+            {/* chef section ends */}
+
+
+
+            {/* about section starts */}
+            <About></About>
+            {/* about section ends */}
+
+            {/* footer section starts */}
+            <Footer></Footer>
+            {/* footer section ends */}
+
 
         </div>
     );
