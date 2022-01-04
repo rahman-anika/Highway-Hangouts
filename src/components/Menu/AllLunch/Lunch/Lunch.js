0dcb9727e0cc6e8752793abd933610dd4fbf75db
rahman-anika <anikarahman37@gmail.com>
2022-01-04 09:20:56 +0600

    menu section updated

--- src/components/Menu/AllLunch/Lunch/Lunch.js
+++ src/components/Menu/AllLunch/Lunch/Lunch.js
@@ -7,14 +7,14 @@ const Lunch = ({ lunch }) => {
     return (
         <>
             <Card className="p-2" style={{ background: "#E3DFDE", marginLeft: "10px", borderRadius: "8px" }}>
-                <Card.Img variant="top" className="img-fluid mx-auto" style={{ width: "200px", height: "200px" }} src={img} />
+                <Card.Img variant="top" className="img-fluid mx-auto" style={{ width: "100%", height: "310px" }} src={img} />
                 <Card.Body>
                     <Card.Title className="fs-2">{name}</Card.Title>
                     <Card.Title className="fs-4">${price}</Card.Title>
                     <Card.Text className="text-start">
-                        {details}
+                        {details.slice(0, 69)}..
                     </Card.Text>
-                    <Link to={`/booking/${_id}`}><Button variant="success">Purchase Now</Button></Link>
+                    <Link to={`/details/${_id}`}><Button variant="success">Show Details</Button></Link>
                 </Card.Body>
             </Card>
         </>
